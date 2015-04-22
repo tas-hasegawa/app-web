868e126dab1e03beec74880e4402eb6113e10f5f
h-hasegawa <h-hasegawa@tads.co.jp>
2015-04-22 23:26:38 +0900

    Added javaDoc

--- src/main/java/app/AppServlet.java
+++ src/main/java/app/AppServlet.java
@@ -5,12 +5,20 @@ import org.glassfish.jersey.servlet.ServletContainer;
 
 import javax.ws.rs.ApplicationPath;
 
+/**
+ * RESTリクエストを受け付けるサーブレットクラスです。
+ *
+ * @author Hasegawa HIROSHI
+ */
 public class AppServlet extends ServletContainer {
 
     public AppServlet() {
         super(new AppConfig());
     }
 
+    /**
+     * JAX-RS(Jersey)における、Resourceの設定クラスです。
+     */
     @ApplicationPath("/resource")
     public static class AppConfig extends ResourceConfig {
         public AppConfig() {
--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -6,19 +6,36 @@ import org.apache.catalina.startup.Tomcat;
 
 import java.io.File;
 
+/**
+ * 組み込みTomcatの実行クラスです。
+ *
+ * @author Hasegawa HIROSHI
+ */
 public class Main {
 
+    /**
+     * Tomcatを起動します。
+     * @param args 実行時引数
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         Tomcat tomcat = new Tomcat();
+
+        // ポートの設定
         tomcat.setPort(8080);
 
+        // コンテキストルートパスを指定
         File base = new File("src/main/webapp/");
         Context context = tomcat.addContext("/app", base.getAbsolutePath());
 
+        // 静的ファイルへのマッピング
         Tomcat.addServlet(context, "default", new DefaultServlet()).addMapping("/static/*");
+
+        // RESTリクエストの窓口となるサーブレットを登録。
         Tomcat.addServlet(context, "jersey-container-servlet", new AppServlet());
         context.addServletMapping("/*", "jersey-container-servlet");
 
+        // Tomcatの起動
         tomcat.start();
         tomcat.getServer().await();
     }
