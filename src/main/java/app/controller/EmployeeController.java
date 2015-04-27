984695d33138f0fe37eef77423e4c2f0d18e5675
h-hasegawa <h-hasegawa@tads.co.jp>
2015-04-27 22:45:49 +0900

    Employeeテーブルにレコードを登録するようにコントローラクラスを修正。

--- src/main/java/app/controller/EmployeeController.java
+++ src/main/java/app/controller/EmployeeController.java
@@ -1,9 +1,12 @@
 package app.controller;
 
+import common.entity.Employee;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -17,6 +20,27 @@ public class EmployeeController {
         EntityManagerFactory fac = Persistence.createEntityManagerFactory("jersey-app");
         EntityManager em = fac.createEntityManager();
         return "employee";
+    }
+
+    @GET
+    @Path("/insert")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String put() {
+        EntityManagerFactory fac = Persistence.createEntityManagerFactory("jersey-app");
+        EntityManager em = fac.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Employee employee = new Employee();
+        employee.setEmployId(1);
+        employee.setMail("h-hasegawa@tads.co.jp");
+        employee.setName("長谷川 寛");
+        employee.setPassword("00000000");
+        em.persist(employee);
+
+        em.getTransaction().commit();
+
+        return "OK";
     }
 
 }
